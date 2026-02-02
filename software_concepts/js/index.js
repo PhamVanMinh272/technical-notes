df2d9ba27f12387d748cace1c057c6efad8472e8
Minh Pham <phamvanminh272@gmail.com>
2026-02-02 23:28:47 +0700

    software concept

---
+++ software_concepts/js/index.js
@@ -0,0 +1,42 @@
+// Interview data (modular, easy to extend)
+const interviews = [
+  { title: "Robustness", file: "concepts/robustness.html" },
+  { title: "Robustness and Reliability", file: "concepts/robustness-reliability.html" },
+  { title: "Interview with Charlie", file: "interviews/interview3.html" }
+];
+
+// Render list dynamically
+const list = document.getElementById("interview-list");
+
+interviews.forEach(interview => {
+  const li = document.createElement("li");
+  li.className = "list-group-item";
+  li.textContent = interview.title;
+
+  li.addEventListener("click", () => {
+    window.location.href = interview.file;
+  });
+
+  list.appendChild(li);
+});
+
+// Quiz
+const quiz = [
+  { title: "Core Python Mastery Quiz", file: "core-python-mastery-quiz.html" },
+  { title: "Software Engineering Skills Quiz", file: "software-engineering-skills-quiz.html" },
+//  { title: "Interview with Charlie", file: "interviews/interview3.html" }
+];
+
+// Render list dynamically
+const quiz_list = document.getElementById("quiz-list");
+quiz.forEach(q => {
+  const li = document.createElement("li");
+  li.className = "list-group-item";
+  li.textContent = q.title;
+
+  li.addEventListener("click", () => {
+    window.location.href = q.file;
+  });
+
+  quiz_list.appendChild(li);
+});
